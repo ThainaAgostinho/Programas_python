4f23ca6755f138871621bbd821c48e41122d2539
ThainaAgostinho <84235286+ThainaAgostinho@users.noreply.github.com>
2021-11-27 22:36:09 -0300

    Update calculador_de_media.py

--- calculador_de_media.py
+++ calculador_de_media.py
@@ -1,4 +1,6 @@
-#Faça	uma	função	que calcule a	média	de	um	aluno	de	acordo	com	o	critério	definido	neste	curso.	Além	disso,	 faça	uma	segunda	 função	que	informe	o	status	do	aluno	de	acordo	com	a	tabela	a	seguir:Nota	acima	de	6	à “Aprovado”Nota	entre	4	e	6	à Conceito	“Verificação	Suplementar”Nota	abaixo	de	4	à Conceito	“Reprovado”#
+#Faça	uma	função	que calcule a	média	de	um	aluno	de	acordo	com	o	critério	definido	neste	curso.	
+#Além	disso,	 faça	uma	segunda	 função	que	informe	o	status	do	aluno	de	acordo	com	a	tabela	a	seguir:#
+#Nota	acima	de	6	à “Aprovado”Nota	entre	4	e	6	à Conceito	“Verificação	Suplementar”Nota	abaixo	de	4	à Conceito	“Reprovado”#
 
 
 print("Olá, aluno! Vamos começar a calcular sua média!")
